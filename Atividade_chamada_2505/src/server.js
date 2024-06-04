9c8ee0875b19adac0ad2282ed9b1db3e89a7c04c
jvgomid3 <161902697+jvgomid3@users.noreply.github.com>
2024-06-03 23:11:08 -0300

    Update server.js

--- Atividade_chamada_2505/src/server.js
+++ Atividade_chamada_2505/src/server.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import routes from "./routes/index.js"
+import cors from 'cors'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use('/api', routes)
